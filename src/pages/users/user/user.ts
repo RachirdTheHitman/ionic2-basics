050a2eb48abb18e9ce8408c769bcf12d61eacabf
YiZhang <hyacinthzy1@gmail.com>
2018-03-09 14:19:09 +1100

    user page added

---
+++ src/pages/users/user/user.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from "@angular/core";
+import {NavParams} from "ionic-angular";
+
+@Component({
+  selector: 'page-user',
+  template: 'user.html'
+})
+export class UserPage implements OnInit{
+  name: string;
+
+  constructor (private navParams: NavParams) {}
+
+  ngOnInit() {
+    this.name = this.navParams.get('userName');
+  }
+
+}
